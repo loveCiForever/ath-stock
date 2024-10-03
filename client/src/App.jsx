25a796df16b4d51abcb06a527b8ed314ada0c5a2
myname <quanghuy71847@gmail.com>
2024-10-04 01:35:50 +0700

    modify ui, add request login api apart

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,39 +1,23 @@
 import React from 'react'
-import athStockLogo from './assets/athStockLogo.png'
-import Form from "./components/Form.jsx"
 
-// 1. Layout classes (e.g., w-1/3, flex, justify-center)
-// 2. Spacing classes (e.g., px-10, py-10, mt-8)
-// 3. Typography classes (e.g., text-lg, font-normal)
-// 4. Background and border classes (e.g., bg-white, border-2)
-// 5. Color classes (e.g., text-orange-400)
-// 6. Other utility classes (e.g., active:scale-[.98], transition-all)
+import athStockLogo from './assets/athStockLogo.png'
+import Login from './components/Login.jsx'
+
+
+
+
 
 export default function App() {
-    return ( 
-        // ---------------------------- Login Page ----------------------------
-        <div 
-            className="
-                flex w-full h-screen justify-evenly items-center
-                px-12 py-12 lg:px-8">
-            <img 
-                src={athStockLogo} 
-                alt="My Company" 
-                className="hidden lg:flex w-1/4 ml-36" />
-            <Form/>
-        </div>
-    //     <div className="flex w-full h-screen">
-    //     <div className="hidden lg:flex h-full w-1/2 items-center justify-center">
-    //       <div className="w-1/2">
-    //         <img src={athStockLogo} alt="athStock logo"/>
-    //       </div>
-    //     </div>
-        
-    //     <div className="flex w-full items-center justify-center lg:w-1/2">
-    //       <Form />
-    //     </div>
-    //   </div>
-
-        // ---------------------------- Login Page ----------------------------
-    );
+  return ( 
+    // ---------------------------- Login Page ----------------------------
+    <div className="flex flex-col lg:w-full h-screen min-w-960
+        justify-center items-center
+        bg-gray-200">
+      <div className="lg:w-1/2 max-w-600 w-1/2
+          justify-center items-center ">
+        <Login/>
+      </div>
+    </div>
+    // ---------------------------- Login Page ----------------------------
+  );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      minWidth: {
+        '480': '480px',
+        '600': '600px',
+        '900': '900px',
+        '960': '960px'
+      },
+      maxWidth: {
+        '600': '600px',
+        '700': '700px'
+      }
+    },
+    variants: {
+      extend: {
+        cursor: ['hover'],
+      },
+    },
   },
   plugins: [],
 }
